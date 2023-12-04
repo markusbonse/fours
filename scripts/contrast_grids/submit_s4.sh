242f1ea61b3f063b7b6f8220882df0200492357a
majobodo <majobodo@web.de>
2023-12-04 11:55:45 +0100

    update submission script

--- scripts/contrast_grids/submit_s4.sh
+++ scripts/contrast_grids/submit_s4.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Read the text file and submit jobs
-while IFS=, read -r DATASET_NAME EXP_ID; do
+while read -r DATASET_NAME EXP_ID; do
     echo "Submitting job for $DATASET_NAME with EXP_ID $EXP_ID"
     sbatch --export=DATASET_NAME="${DATASET_NAME}",EXP_ID="${EXP_ID}" --job-name="S4_${DATASET_NAME}_ID_${EXP_ID}" compute_residuals_s4.slurm
 done < $1
