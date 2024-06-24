61a9f756bbe308687ccbb56ff74246adb0a50a7d
majobodo <majobodo@web.de>
2024-06-24 18:13:13 +0200

    Clean up the PCA noise model code

--- fours/position_flux/pca.py
+++ fours/position_flux/pca.py
@@ -2,7 +2,6 @@ import numpy as np
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 from fours.models.rotation import FieldRotationModel
 
@@ -31,20 +30,14 @@ class PCANoiseModel(nn.Module):
             inverse=False,
             register_grid=True)
 
-    def compute_basis(
-            self,
-            images):
+        # create an empty basis
+        self.register_buffer(
+            "basis",
+            torch.zeros(self.m_image_shape ** 2, self.m_pca_number))
 
-        if self.m_approx_svd == -1:
-            _, _, basis = torch.linalg.svd(images)
-            basis = basis.T
-        else:
-            _, _, basis = torch.svd_lowrank(
-                images, niter=1, q=self.m_approx_svd)
-
-        return basis
-
-    def forward(self, images):
+    @staticmethod
+    def _prepare_frames(
+            images: torch.Tensor):
         # 1.) reshape images to fit for PCA
         images = images.view(images.shape[0],
                              images.shape[1] * images.shape[2])
@@ -52,22 +45,43 @@ class PCANoiseModel(nn.Module):
         # 2.) remove the mean as needed for PCA
         images = images - images.mean(dim=0)
 
-        # 3.) compute PCA basis
-        basis = self.compute_basis(images)
+        return images
 
-        # 4.) compute PCA residuals
-        pca_rep = torch.matmul(images, basis[:, :self.m_pca_number])
-        noise_estimate = torch.matmul(pca_rep, basis[:, :self.m_pca_number].T)
+    def update_noise_model(
+            self,
+            images):
+        # 1.) prepare the frames
+        images = self._prepare_frames(images)
+
+        # 2.) compute the PCA basis
+        if self.m_approx_svd == -1:
+            _, _, basis = torch.linalg.svd(images)
+            basis = basis.T
+        else:
+            _, _, basis = torch.svd_lowrank(
+                images, niter=1, q=self.m_approx_svd)
+
+        # 3.) save the basis
+        self.basis = basis.detach()
+
+    def forward(self, images):
+        # 1.) prepare the frames
+        images = self._prepare_frames(images)
+
+        # 2.) compute PCA residuals
+        pca_rep = torch.matmul(images, self.basis[:, :self.m_pca_number])
+        noise_estimate = torch.matmul(pca_rep,
+                                      self.basis[:, :self.m_pca_number].T)
         residual = images - noise_estimate
         residual_sequence = residual.view(
             images.shape[0],
             self.m_image_shape,
             self.m_image_shape)
 
-        # 5.) rotate the frames
+        # 3.) rotate the frames
         rotated_frames = self.rotation_model(
             residual_sequence.unsqueeze(1).float(),
             parang_idx=torch.arange(len(residual_sequence))).squeeze(1)
 
-        # 6.) average along the time axis
+        # 4.) average along the time axis
         return rotated_frames.mean(dim=0)
