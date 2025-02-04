0885a2c0f86dfc9c501cd1bd1893035db8ddaa86
majobodo <majobodo@web.de>
2025-02-04 15:37:01 +0100

    Refactor and document data loading functions.
    Updated `load_adi_data` with type hints, detailed docstrings, and reformatted arguments for better readability. Added a docstring to `read_fours_root_dir` describing its purpose, return value, and potential exceptions.

--- fours/utils/data_handling.py
+++ fours/utils/data_handling.py
@@ -1,22 +1,36 @@
 import os
 import numpy as np
 import h5py
+from typing import Tuple
 
 from astropy.io import fits
 
 
-def load_adi_data(hdf5_dataset: str,
-                  data_tag: str,
-                  psf_template_tag: str,
-                  para_tag="PARANG"):
+def load_adi_data(
+        hdf5_dataset: str,
+        data_tag: str,
+        psf_template_tag: str,
+        para_tag: str = "PARANG"
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
-    Function to load ADI data from hdf5 files
-    :param hdf5_dataset: The path to the hdf5 file
-    :param data_tag: Tag of the science data
-    :param psf_template_tag: Tag of the PSF template
-    :param para_tag: Tag of the parallactic angles
-    :return: Tuple (Science, adi_angles, template)
+    Loads Angular Differential Imaging (ADI) data from an HDF5 dataset. 
+    See "Examples" in the Documentation.
+
+    Args:
+        hdf5_dataset: Path to the HDF5 file containing the data.
+        data_tag: HDF5 key for the science data cube (e.g., images at 
+            multiple wavelengths or observations).
+        psf_template_tag: HDF5 key for the PSF template data.
+        para_tag: HDF5 key for the parallactic angles array. Default is 
+            "PARANG".
+
+    Returns:
+        A tuple containing:
+        - data: A numpy array representing the science data cube.
+        - angles: A numpy array of parallactic angles in radians.
+        - psf_template_data: A numpy array representing the PSF template.
     """
+    
     hdf5_file = h5py.File(hdf5_dataset, 'r')
     data = hdf5_file[data_tag][...]
     angles = np.deg2rad(hdf5_file[para_tag][...])
@@ -47,6 +61,21 @@ def save_as_fits(
 
 
 def read_fours_root_dir() -> str:
+    """
+    Reads the root directory for the FOURS dataset from an environment variable.
+
+    The function checks if the `FOURS_ROOT_DIR` environment variable is correctly 
+    set to a valid directory path, prints its location, and returns it. Raises an 
+    IOError if the directory does not exist.
+
+    Returns:
+        A string representing the path to the FOURS_ROOT_DIR.
+
+    Raises:
+        IOError: If the directory specified in the `FOURS_ROOT_DIR` environment 
+        variable does not exist.
+    """
+
     root_dir = os.getenv('FOURS_ROOT_DIR')
 
     if not os.path.isdir(root_dir):
