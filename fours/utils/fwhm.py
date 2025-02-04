c943ac1e7b3023746f54b602812e8978b6ae7683
majobodo <majobodo@web.de>
2025-02-04 15:38:38 +0100

    Refactor `get_fwhm` to include type hints and docstring.
    Enhanced the function with type annotations and a detailed docstring for improved clarity and maintainability. The changes specify input and output types and provide a concise explanation of the function's purpose and behavior.

--- fours/utils/fwhm.py
+++ fours/utils/fwhm.py
@@ -4,7 +4,21 @@ from astropy.modeling.functional_models import Moffat2D
 from astropy.modeling import fitting
 
 
-def get_fwhm(psf_template):
+def get_fwhm(
+        psf_template: np.ndarray
+) -> float:
+    """
+    Compute the Full Width at Half Maximum (FWHM) of a 2D point spread 
+    function (PSF) template using a Moffat model.
+
+    Parameters:
+    psf_template (np.ndarray): A 2D array representing the PSF intensity 
+                                distribution.
+
+    Returns:
+    float: The computed FWHM value, rounded to one decimal place.
+    """
+    
     # Fit the data using astropy.modeling
     p_init = Moffat2D(
         amplitude=np.max(psf_template),
